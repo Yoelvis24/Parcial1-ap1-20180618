dc124b40c8bd0842af6045378b67ad3b396c702c
Yoelvis Rafael Perez Brito <yoelvis.perez.20017@gmail.com>
2021-02-08 19:25:19 -0400

    Creando la interfaz gráfica

--- Parcial1-ap1-20180618/UI/Registros/RegistroCiudades.Designer.cs
+++ Parcial1-ap1-20180618/UI/Registros/RegistroCiudades.Designer.cs
@@ -29,13 +29,116 @@ namespace Parcial1_ap1_20180618
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.IdCiudadLabel = new System.Windows.Forms.Label();
+            this.NombreLabel = new System.Windows.Forms.Label();
+            this.NombreTextBox = new System.Windows.Forms.TextBox();
+            this.IdCiudadNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.BuscarButton = new System.Windows.Forms.Button();
+            this.NuevoButton = new System.Windows.Forms.Button();
+            this.GuardarButton = new System.Windows.Forms.Button();
+            this.EliminarButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.IdCiudadNumericUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // IdCiudadLabel
+            // 
+            this.IdCiudadLabel.AutoSize = true;
+            this.IdCiudadLabel.Location = new System.Drawing.Point(12, 18);
+            this.IdCiudadLabel.Name = "IdCiudadLabel";
+            this.IdCiudadLabel.Size = new System.Drawing.Size(58, 15);
+            this.IdCiudadLabel.TabIndex = 0;
+            this.IdCiudadLabel.Text = "Ciudad Id";
+            // 
+            // NombreLabel
+            // 
+            this.NombreLabel.AutoSize = true;
+            this.NombreLabel.Location = new System.Drawing.Point(12, 77);
+            this.NombreLabel.Name = "NombreLabel";
+            this.NombreLabel.Size = new System.Drawing.Size(51, 15);
+            this.NombreLabel.TabIndex = 1;
+            this.NombreLabel.Text = "Nombre";
+            // 
+            // NombreTextBox
+            // 
+            this.NombreTextBox.Location = new System.Drawing.Point(12, 95);
+            this.NombreTextBox.Name = "NombreTextBox";
+            this.NombreTextBox.Size = new System.Drawing.Size(229, 23);
+            this.NombreTextBox.TabIndex = 2;
+            // 
+            // IdCiudadNumericUpDown
+            // 
+            this.IdCiudadNumericUpDown.Location = new System.Drawing.Point(12, 36);
+            this.IdCiudadNumericUpDown.Name = "IdCiudadNumericUpDown";
+            this.IdCiudadNumericUpDown.Size = new System.Drawing.Size(120, 23);
+            this.IdCiudadNumericUpDown.TabIndex = 3;
+            // 
+            // BuscarButton
+            // 
+            this.BuscarButton.Location = new System.Drawing.Point(138, 34);
+            this.BuscarButton.Name = "BuscarButton";
+            this.BuscarButton.Size = new System.Drawing.Size(75, 23);
+            this.BuscarButton.TabIndex = 4;
+            this.BuscarButton.Text = "Buscar";
+            this.BuscarButton.UseVisualStyleBackColor = true;
+            // 
+            // NuevoButton
+            // 
+            this.NuevoButton.Location = new System.Drawing.Point(12, 137);
+            this.NuevoButton.Name = "NuevoButton";
+            this.NuevoButton.Size = new System.Drawing.Size(90, 51);
+            this.NuevoButton.TabIndex = 5;
+            this.NuevoButton.Text = "Nuevo";
+            this.NuevoButton.UseVisualStyleBackColor = true;
+            // 
+            // GuardarButton
+            // 
+            this.GuardarButton.Location = new System.Drawing.Point(123, 137);
+            this.GuardarButton.Name = "GuardarButton";
+            this.GuardarButton.Size = new System.Drawing.Size(90, 51);
+            this.GuardarButton.TabIndex = 6;
+            this.GuardarButton.Text = "Guardar";
+            this.GuardarButton.UseVisualStyleBackColor = true;
+            // 
+            // EliminarButton
+            // 
+            this.EliminarButton.Location = new System.Drawing.Point(238, 137);
+            this.EliminarButton.Name = "EliminarButton";
+            this.EliminarButton.Size = new System.Drawing.Size(90, 51);
+            this.EliminarButton.TabIndex = 7;
+            this.EliminarButton.Text = "Eliminar";
+            this.EliminarButton.UseVisualStyleBackColor = true;
+            // 
+            // RegistroCiudades
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(340, 211);
+            this.Controls.Add(this.EliminarButton);
+            this.Controls.Add(this.GuardarButton);
+            this.Controls.Add(this.NuevoButton);
+            this.Controls.Add(this.BuscarButton);
+            this.Controls.Add(this.IdCiudadNumericUpDown);
+            this.Controls.Add(this.NombreTextBox);
+            this.Controls.Add(this.NombreLabel);
+            this.Controls.Add(this.IdCiudadLabel);
+            this.Name = "RegistroCiudades";
+            this.Text = "Registro de Ciudades";
+            ((System.ComponentModel.ISupportInitialize)(this.IdCiudadNumericUpDown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label IdCiudadLabel;
+        private System.Windows.Forms.Label NombreLabel;
+        private System.Windows.Forms.TextBox NombreTextBox;
+        private System.Windows.Forms.NumericUpDown IdCiudadNumericUpDown;
+        private System.Windows.Forms.Button BuscarButton;
+        private System.Windows.Forms.Button NuevoButton;
+        private System.Windows.Forms.Button GuardarButton;
+        private System.Windows.Forms.Button EliminarButton;
     }
 }
 
