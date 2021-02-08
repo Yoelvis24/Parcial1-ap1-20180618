8db95299a97c8d7872855e1a13c05fe3aad77914
Yoelvis Rafael Perez Brito <yoelvis.perez.20017@gmail.com>
2021-02-08 19:13:01 -0400

    Creando CiudadesBLL

---
+++ Parcial1-ap1-20180618/BLL/CiudadesBLL.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using Parcial1_ap1_20180618.Entidades;
+using Parcial1_ap1_20180618.DAL;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Parcial1_ap1_20180618.BLL
+{
+    public class CiudadesBLL
+    {
+        public static bool Existe(int id)
+        {
+            Contexto contexto = new Contexto();
+            bool encontrado = false;
+
+            try
+            {
+                encontrado = contexto.Ciudades.Any(e => e.CiudadId == id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return encontrado;
+        }
+
+        public static bool Eliminar(int id)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                var ciudades = contexto.Ciudades.Find(id);
+                if(ciudades != null)
+                {
+                    contexto.Ciudades.Remove(ciudades);
+                    paso = contexto.SaveChanges() > 0;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return paso;
+        }
+
+        public static Ciudades Buscar(int id)
+        {
+            Contexto contexto = new Contexto();
+            Ciudades ciudades;
+            try
+            {
+                ciudades = contexto.Ciudades.Find(id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return ciudades;
+        }
+
+        public static bool Insertar(Ciudades ciudades)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                contexto.Ciudades.Add(ciudades);
+                paso = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return paso;
+        }
+
+        public static bool Modificar(Ciudades ciudades)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                contexto.Entry(ciudades).State = EntityState.Modified;
+                paso = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return paso;
+        }
+
+        public static bool Guardar(Ciudades ciudades)
+        {
+            if (!Existe(ciudades.CiudadId))
+                return Insertar(ciudades);
+            else
+                return Modificar(ciudades);
+        }
+    }
+}
