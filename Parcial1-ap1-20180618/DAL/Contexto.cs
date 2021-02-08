470f625a954ae2fed7410708ee752be79753aaa1
Yoelvis Rafael Perez Brito <yoelvis.perez.20017@gmail.com>
2021-02-08 18:41:21 -0400

    Creando Contexto

---
+++ Parcial1-ap1-20180618/DAL/Contexto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Parcial1_ap1_20180618.Entidades;
+using Microsoft.EntityFrameworkCore;
+
+namespace Parcial1_ap1_20180618.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Ciudades> Ciudades { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source = GestionCiudades.Db");
+        }
+    }
+}
